b7f3c0c691cd5c34b95413a1025998a594ad4ff2
Alin Jerpelea <jerpelea@gmail.com>
2012-11-15 20:37:04 +0200

    mesona: small updates

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,10 +6,8 @@ USE_CAMERA_STUB := true
 #inherit from the common tamsui definitions
 -include device/sony/tamsui-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_SOURCE := kernel/sony/msm7x27a
-TARGET_KERNEL_CONFIG := cyanogen_tapioca_defconfig
-#TARGET_PREBUILT_KERNEL := device/sony/mesona/prebuilt/kernel
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+TARGET_KERNEL_CONFIG := cyanogen_mesona_defconfig
+BOARD_KERNEL_CMDLINE :=
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 16
@@ -18,8 +16,8 @@ BOARD_VOLD_MAX_PARTITIONS := 16
 # since these partitions are not visible
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
-
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 524288000
+#681574400
 BOARD_USERDATA_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 131072
 
