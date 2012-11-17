55a704b03c4ff713c8c28c602516e0a05a0b37fb
Alin Jerpelea <jerpelea@gmail.com>
2012-11-17 16:41:51 +0200

    mesona:  animation resource fix

--- cm.mk
+++ cm.mk
@@ -8,7 +8,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST23i_1257-4009 BUILD_FINGERPRINT=SEMC/ST23i_1257-4009/ST23i:4.0.4/6.1.A.0.452/O_5_zw:user/release-keys PRIVATE_BUILD_DESC="ST23i-user 4.0.4 6.1.A.0.452 O_5_zw test-keys"
 
-TARGET_BOOTANIMATION_NAME := 160
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
 
 PRODUCT_NAME := cm_mesona
 PRODUCT_DEVICE := mesona
