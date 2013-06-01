6472351f51515103a35959b6c0e3a90ff7eeaa72
Alin Jerpelea <jerpelea@gmail.com>
2013-06-01 10:21:54 +0300

    mesona: move wifi setup to device
    Change-Id: Ie08a2870aac5f68e76059ed4479cf15b51fe3028

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,20 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/mesona/include
 TARGET_KERNEL_CONFIG := cm_tamsui_mes_defconfig
 BOARD_KERNEL_CMDLINE := device/sony/mesona/config/cmdline.txt
 
+# Wifi related defines
+BOARD_WLAN_DEVICE                := bcmdhd
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/bcm4330/sdio_g_pool_pno_pktfilter_keepalive_wapi_wme_idsup_idauth_sta_aoe.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/bcm4330/sdio_g_pool_pno_pktfilter_keepalive_wapi_wme_idsup_idauth_apsta_aoe.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/bcm4330/sdio_g_pool_pno_pktfilter_keepalive_wapi_wme_idsup_idauth_p2p_aoe.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+
+BOARD_LEGACY_NL80211_STA_EVENTS  := true
+
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 16
 
